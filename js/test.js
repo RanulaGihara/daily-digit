4a3f6b559d19ec0e1fc0b933af1512c9aa1a08c5
ranulagihara <ranulaw@opusxenta.com>
2024-10-14 23:38:17 +0530

    CHANGED

--- js/test.js
+++ js/test.js
@@ -1,4 +1,23 @@
 const person = { name: "Alice" };
 const anotherPerson = person;
 anotherPerson.name = "Bob";
-console.log(person.name);
+
+//Q2: Write a function mergeObjects that takes two objects and merges them into one. If both object
+
+const nds = handleObjs()
+console.log(nds);
+
+function handleObjs ()  {
+    const obj1 = { a: 1, b: 2 };
+    const obj2 = { b: 3, c: 4 };
+    for (const key in obj2) {
+        for (const key in obj1) {
+            if (obj1[key] === obj2[key]) {
+                obj1[key] = obj2[key];
+            }
+        }
+    }
+    return obj1
+};
+
+//In JavaScript, functions declared with const or let are not hoisted the same way as function declarations (using the function keyword).
