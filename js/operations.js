e2b80cf08e362c98d6a5c0875efc6412f39d25b0
ranulagihara <ranulaw@opusxenta.com>
2024-10-29 22:21:37 +0530

    added

--- js/operations.js
+++ js/operations.js
@@ -6,12 +6,41 @@ console.log(name2);
 
 // ----------------The ?? operator checks if the value on its left is either null or undefined. If it is, it returns the value on the right.---------
 
+
+
+
+
+
 // || - Logical OR
 let a = 0;
 let b = 20;
 console.log(a || b);
 
 // ----------------The || operator checks if the value on the left is "falsy" (like null, undefined, 0, false, NaN, or ""). If it is, it returns the right-side value; otherwise, it returns the left-side value. This is useful for setting defaults if a variable might be undefined or another falsy value.---------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // && - Logical AND
 let c = 0;
@@ -20,3 +49,13 @@ console.log(c && d);
 
 //--------------------If the first operand is "falsy," JavaScript stops evaluating and returns that "falsy" value. This behavior is called "short-circuiting."
 // Conditional Rendering in User Interfaces
+
+//6. Short-Circuiting to Avoid Unnecessary Code Execution
+isCached = false;
+
+// Only execute a complex calculation if caching is disabled
+!isCached && performComplexCalculation();
+
+function performComplexCalculation() {
+    console.log("Performing complex calculation...");
+}
